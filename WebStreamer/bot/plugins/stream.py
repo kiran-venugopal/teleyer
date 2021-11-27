acd45a0aca96c161551669c7a818dcd5263b092f
Kiran <kiran.venugopal@actiotech.com>
2021-11-27 17:20:03 +0530

    initial

---
+++ WebStreamer/bot/plugins/stream.py
@@ -0,0 +1,38 @@
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+
+from pyrogram import filters
+from WebStreamer.vars import Var
+from urllib.parse import quote_plus
+from WebStreamer.bot import StreamBot
+from pyrogram.types.messages_and_media import message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+def detect_type(m: Message):
+    if m.document:
+        return m.document
+    elif m.video:
+        return m.video
+    elif m.audio:
+        return m.audio
+    else:
+        return
+    
+
+@StreamBot.on_message(filters.document | filters.video | filters.audio, group=4)
+async def media_receive_handler(_, m: Message):
+    print(m)
+    file = detect_type(m)
+    file_name = ''
+    if file:
+        file_name = file.file_name
+    log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
+    url=Var.URL
+    if Var.CLIENT_URL:
+        url=Var.CLIENT_URL
+    stream_link = url + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
+    await m.reply_text(
+        text="`{}`".format(stream_link),
+        quote=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Open', url=stream_link)]])
+    )
