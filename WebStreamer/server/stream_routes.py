8654e12c9e489e0a3efc03cad99bbd4b9509a129
Kiran <kiran.venugopal@actiotech.com>
2021-11-27 19:49:21 +0530

    header added

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -73,6 +73,7 @@ async def media_streamer(request, message_id: int):
             "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
             "Content-Disposition": f'attachment; filename="{file_name}"',
             "Accept-Ranges": "bytes",
+            'Access-Control-Allow-Origin', '*'
         }
     )
 
