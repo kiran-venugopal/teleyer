06327e4be5d6e487358d662015bb01b1673d9733
Kiran <kiran.venugopal@actiotech.com>
2022-01-15 17:44:58 +0530

    minor correction

--- WebStreamer/db/config.py
+++ WebStreamer/db/config.py
@@ -1,6 +1,7 @@
+from pymongo import MongoClient
+from WebStreamer.vars import Var
+
 def get_database():
-    from pymongo import MongoClient
-    from WebStreamer.vars import Var
     CONNECTION_STRING = "mongodb+srv://teleyer:"+ Var.DB_PASSWORD +"@cluster0.dhmyo.mongodb.net/message_db?retryWrites=true&w=majority"
     client = MongoClient(CONNECTION_STRING)
     db = client['message_db']
--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -37,19 +37,26 @@ async def files(request):
     # print(messages)
     response = []
     for message in messages:
-        mesg_obj = {}
+        if not message:
+            continue
+
         doc = message["document"]
-        
+
+        if not doc:
+            continue
+
+        mesg_obj = {}
+
         mesg_obj["message_id"] = message["message_id"]
         mesg_obj["file_name"] = doc["file_name"]
         mesg_obj["date"] = message["date"]
         mesg_obj["file_size"] = doc["file_size"]
+
         if doc["thumbs"]:
             mesg_obj["thumb_id"] = doc["thumbs"][0]["file_id"]
 
         response.append(mesg_obj)
-        response.reverse()
-  
+    response.reverse()
     return web.json_response(response, headers={ "Access-Control-Allow-Origin": "*"})
 
 
